a0bd984181bbf613460f3070bd3c87c48be8d2fe
CD-MCV <84226164+Cooldude0340@users.noreply.github.com>
2021-06-07 15:58:53 +0530

    Update anilist.py

--- lazyleech/plugins/anilist.py
+++ lazyleech/plugins/anilist.py
@@ -1,14 +1,25 @@
-"""	
-	Anilist Search Plugin for Userbot	
-	Usage : .anilist animeName	
-	By :- @Zero_cool7870	
-"""
+'''
+# lazyleech - Telegram bot primarily to leech from torrents and upload to Telegram
+# Copyright (c) 2021 lazyleech developers <theblankx protonmail com, meliodas_bot protonmail com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
 
 import requests 
 import re
 import json
 import asyncio
-from ULTRA import CMD_HELP
 from ULTRA.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 async def callAPI(search_str):
